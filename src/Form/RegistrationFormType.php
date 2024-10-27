4c8290de734683c10ea928ca73c0552affbbaa57
Lois <loissambade@gmail.com>
2024-10-27 12:44:21 +0100

    un poco de estilo al form de login

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Usuario;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -17,8 +18,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('usuario')
-            ->add('email')
+            ->add('usuario', TextType::class,[
+                'required'=>true,
+                'attr'=>['class'=>'form-control input-primario']])
+            ->add('email', TextType::class,[
+                'required'=>true,
+                'attr'=>['class'=>'form-control input-primario']])
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
@@ -26,12 +31,13 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'attr'=>['class'=>'']
             ])
             ->add('plainPassword', PasswordType::class, [
                                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'class'=>'form-control input-primario'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
